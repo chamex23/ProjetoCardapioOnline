5d9becdcda212c6d44c0d48a20310696573d17b7
chamex23 <monteirokaka15@gmail.com>
2023-10-24 13:44:14 -0300

    commit site

---
+++ js/dados.js
@@ -0,0 +1,387 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "espetoCarne",
+            "img": "./img/Espetinhos/carne.jpg",
+            "name": "CARNE",
+            "dsc": "Espeto de carne",
+            "price": 7
+        },
+        {
+            "id": "espetoFrango",
+            "img": "./img/Espetinhos/frango.jpg",
+            "name": "FRANGO",
+            "dsc": "Espeto de frango",
+            "price": 7
+        },
+        {
+            "id": "espetoCoxinhaAsa",
+            "img": "./img/Espetinhos/coxinhadaasa.jpg",
+            "name": "COXINHA DA ASA",
+            "dsc": "Espeto da coxa da asa",
+            "price": 7
+        },
+        {
+            "id": "espetoCalabresa",
+            "img": "./img/Espetinhos/calabresa.jpeg",
+            "name": "CALABRESA",
+            "dsc": "Espeto de calabresa",
+            "price": 7
+        },
+        {
+            "id": "espetoDefumado",
+            "img": "./img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
+            "name": "ESPETO DEFUMADO",
+            "dsc": "Espeto defumado",
+            "price": 7
+        },
+        {
+            "id": "espetoApimentado",
+            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
+            "name": "ESPETO APIMENTADO",
+            "dsc": "Espeto apimentado",
+            "price": 7
+        },
+        {
+            "id": "espetoFarofaVinagrete",
+            "img": "./img/Espetinhos/completo.jpg",
+            "name": "COMPLETO",
+            "dsc": "Espeto completo, com farofa e vinagrete",
+            "price": 10
+        },
+       
+    ],
+    "burgers": [
+        {
+            "id": "lenha",
+            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg",
+            "name": "LENHA",
+            "dsc": "Pão, Carne artesanal 100g, Queijo, Salada, Tomate",
+            "price": 12
+        },
+        {
+            "id": "carvao",
+            "img": "./img/cardapio/burguers/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.jpg",
+            "name": "CARVÃO",
+            "dsc": "Pão, Carne artesanal 100g, Frango, Queijo, Salada, Tomate",
+            "price": 14
+        },
+        {
+            "id": "fumaca",
+            "img": "./img/cardapio/burguers/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg",
+            "name": "FUMAÇA",
+            "dsc": "Pão, Carne artesanal 100g, Bacon, Queijo, Cebola caramelizada, Salada, Tomate",
+            "price": 15
+        },
+        {
+            "id": "faisca",
+            "img": "./img/cardapio/burguers/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg",
+            "name": "FAÍSCA",
+            "dsc": "Pão, Carne artesanal 100g, Calabresa, Queijo, Salada, Tomate, Molho Barbecue",
+            "price": 15
+        },
+        {
+            "id": "brasa",
+            "img": "./img/cardapio/burguers/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.jpg",
+            "name": "BRASA",
+            "dsc": "Pão, Carne artesanal 100g, Frango, Calabresa, Queijo, Barbecue, Salada, Tomate",
+            "price": 17
+        },
+        {
+            "id": "fogo",
+            "img": "./img/cardapio/burguers/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.jpg",
+            "name": "FOGO",
+            "dsc": "Pão, Carne artesanal 100g, Bacon, Calabresa, Queijo, Salada, Tomate, Cebola caramelizada, Molho picante",
+            "price": 18
+        },
+        {
+            "id": "braseiro",
+            "img": "./img/cardapio/burguers/handf-double-stack-burger-kit-for-4.4ee9f54b1d6087e9996335f07c13e5cd.jpg",
+            "name": "BRASEIRO",
+            "dsc": "Pão, Carne artesanal 100g, Frango, Bacon, Calabresa, Queijo, Tomate, Cebola caramelizada, Molho picante",
+            "price": 20
+        },
+        {
+            "id": "brasao",
+            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
+            "name": "BRASÃO",
+            "dsc": "Pão, 2x Carne artesanal 100g, Frango, Bacon, Calabresa, 2x Queijo, Molho Barbecue, Tomate, Salada",
+            "price": 25
+        },
+      
+    ],
+    "sobremesas": [
+        {
+            "id": "adicionalCarne",
+            "img": "./img/Adicionais/carne.jpg",
+            "name": "CARNE",
+            "dsc": "Adicione mais uma carne no seu hambúguer",
+            "price": 6
+        },
+        {
+            "id": "adicionalFrango",
+            "img": "./img/Adicionais/frango-a-milanesa-1658511982361_v2_4x3.jpg",
+            "name": "FRANGO",
+            "dsc": "Adicione mais pedaços de frango no seu hambúguer",
+            "price": 3
+        },
+        {
+            "id": "adicionalBacon",
+            "img": "./img/Adicionais/bacon.jpg",
+            "name": "BACON",
+            "dsc": "Adicione mais bacon no seu hambúguer",
+            "price": 4
+        },
+        {
+            "id": "adicionalCalabresa",
+            "img": "./img/Adicionais/calabresa.jpg",
+            "name": "CALABRESA",
+            "dsc": "Adicione mais pedaços de calabresa no seu hambúguer",
+            "price": 4
+        },
+        {
+            "id": "adicionalQueijo",
+            "img": "./img/Adicionais/queijo.jpg",
+            "name": "QUEIJO",
+            "dsc": "Adicione mais queijo no seu hambúguer",
+            "price": 4
+        },
+        {
+            "id": "adicionalCebolaCaramelizada",
+            "img": "./img/Adicionais/cebola caramelizada.jpg",
+            "name": "CEBOLA CARAMELIZADA",
+            "dsc": "Adicione mais Cebola caramelizada",
+            "price": 2
+        },
+        {
+            "id": "adicionalMolhoBarbecue",
+            "img": "./img/Adicionais/barbecue.jpg",
+            "name": "MOLHO BARBECUE",
+            "dsc": "Adicional de Molho Barbecue",
+            "price": 2
+        },
+        {
+            "id": "adicionalMolhoPicante",
+            "img": "./img/Adicionais/molho picante.jpg",
+            "name": "MOLHO PICANTE",
+            "dsc": "Adicione mais Molho picante",
+            "price": 2
+        },
+        {
+            "id": "adicionalMaioneseDaCasa",
+            "img": "./img/Adicionais/Maionesecaseira.jpg",
+            "name": "MAIONESE DA CASA",
+            "dsc": "Adicione mais um sachê de maionese da casa",
+            "price": 1
+        },
+    ],
+    "bebidas": [
+        {
+            "id": "cocacolaLitro",
+            "img": "./img/Bebidas/Cocacola1lt.jpg",
+            "name": "COCA-COLA 1LT",
+            "dsc": "...",
+            "price": 7
+        },
+        {
+            "id": "guaranaLitro",
+            "img": "./img/Bebidas/Guaranalt.jpg",
+            "name": "GUARANÁ 1LT",
+            "dsc": "...",
+            "price": 7
+        },
+        {
+            "id": "cocacolaLata",
+            "img": "./img/Bebidas/cocacolalata.jpg",
+            "name": "COCA-COLA LATA",
+            "dsc": "...",
+            "price": 4
+        },
+        {
+            "id": "guaranaLata",
+            "img": "./img/Bebidas/guaranalata.png",
+            "name": "GUARANÁ LATA",
+            "dsc": "...",
+            "price": 4
+        },
+        {
+            "id": "bebidasAgua",
+            "img": "./img/Bebidas/aguamineral.jpg",
+            "name": "ÁGUA",
+            "dsc": "...",
+            "price": 2
+        },
+    ],
+    "pizzas": [
+        {
+            "id": "2-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/2-lou-malnatis-deep-dish-pizzas.bf0fe065d251a9cca3925b269d443a27.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "2 Lou Malnati's Deep Dish Pizzas",
+            "price": 67.99
+        },
+        {
+            "id": "23699-choose-your-own-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-thin-crust-pizza-4-pack.b928a2008eab50c65dc87e59b5952190.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Thin Crust Pizza - 4 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-new-haven-style-pizza-6-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-new-haven-style-pizza-6-pack.ab82828afc6172cdd4017556c15e36dd.jpg",
+            "name": "Zuppardi's Apizza",
+            "dsc": "New Haven-Style Pizza - 6 Pack (Choose Your Own)",
+            "price": 79
+        },
+        {
+            "id": "6-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/6-lou-malnatis-deep-dish-pizzas.f59993181da5d295668c8a6fb856055e.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "6 Lou Malnati's Deep Dish Pizzas",
+            "price": 116.99
+        },
+        {
+            "id": "wood-fired-pizzas-best-seller-4-pack",
+            "img": "./img/cardapio/pizzas/wood-fired-pizzas-best-seller-4-pack.1653bb05922ba153ac178f8365d27f6d.jpg",
+            "name": "Pizzeria Bianco",
+            "dsc": "Wood Fired Pizzas Best Seller - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "236991-choose-your-own-deep-dish-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-deep-dish-pizza-3-pack.4111791511244a4946bb5c9ad2c17da9.jpg",
+            "name": "Bartolini's",
+            "dsc": "Choose Your Own Deep Dish Pizza - 3 Pack",
+            "price": 139
+        },
+        {
+            "id": "choose-your-own-detroit-style-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/detroit-style-pizza-choose-your-own-3-pack.6b6f4909ffd4066d5471e70eac5c3d89.jpg",
+            "name": "Emmy Squared",
+            "dsc": "Detroit-Style Pizza - Choose Your Own 3 Pack",
+            "price": 89
+        },
+        {
+            "id": "brooklyn-pizza-choose-your-own-5-pack",
+            "img": "./img/cardapio/pizzas/brooklyn-pizza-choose-your-own-5-pack.edc4f476a75207d0af840ce6f225f2b3.jpg",
+            "name": "Paesan's Pizza",
+            "dsc": "Brooklyn Pizza - Choose Your Own 5 Pack",
+            "price": 69
+        },
+        {
+            "id": "choose-your-own-chicago-deep-dish-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/chicago-deep-dish-pizza-4-pack.49927daafa8c147fe9bb2a113e56668e.jpg",
+            "name": "My Pi Pizza",
+            "dsc": "Chicago Deep Dish Pizza - 4 Pack",
+            "price": 129
+        },
+        {
+            "id": "4-lou-malnatis-deep-dish-pizzas",
+            "img": "./img/cardapio/pizzas/4-lou-malnatis-deep-dish-pizzas.8c79eb7506b5752ab3387d8174246b17.jpg",
+            "name": "Lou Malnati's Pizza",
+            "dsc": "4 Lou Malnati's Deep Dish Pizzas",
+            "price": 96.99
+        },
+        {
+            "id": "tonys-custom-pizza-3-pack",
+            "img": "./img/cardapio/pizzas/choose-your-own-pizza-3-pack.fcf7a43e38593007ef2857fe16d6dd26.jpg",
+            "name": "Tony's Pizza Napoletana",
+            "dsc": "Choose Your Own Pizza - 3 Pack",
+            "price": 99
+        },
+        {
+            "id": "plain-thin-crust-pizza-4-pack",
+            "img": "./img/cardapio/pizzas/plain-thin-crust-pizza-4-pack.5540e9d166db2f0853643c6517e4a225.jpg",
+            "name": "The Columbia Inn",
+            "dsc": "Plain Thin Crust Pizza - 4 Pack",
+            "price": 79
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
